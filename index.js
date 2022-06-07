526157824e5b1d5fb7f49ece080cdd2d9ece4a06
sid-al <sendplusx@gmail.com>
2022-06-07 10:16:57 +0300

    add function

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // Code your solution in this file!
-
-function distanceFromHqInBlocks(distance){
-    return distance = 1;
+const HQBlocks = 42;
+//const fromHQ = num2;
+function distanceFromHqInBlocks(currentLocation){
+     let distanceInBlocks = HQBlocks - currentLocation ;
+     return Math.abs(distanceInBlocks);
+}
+function distanceFromHqInFeet(currentLocationInFeet){
+    let distanceInFeet = ((HQBlocks - currentLocationInFeet)*264);
+    return Math.abs((distanceInFeet));
 }
 
-function distanceFromHqInBlocks(distance){
-
-}
+func
